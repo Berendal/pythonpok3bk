835c44f15cecf7f4503506eedc8e933f9c8ea456
Dead Tapok <volot89@yandex.ru>
2023-02-23 14:59:58 +0300

    newprob

--- mod9.py
+++ mod9.py
@@ -1,5 +1,11 @@
-#next commit
-
+def exception_decorator(func):
+    def wrapper(*args, **kwargs):
+        try:
+            res = func(*args, **kwargs)
+            return (res, 'Функция выполнилась без ошибок')
+        except:
+            return (None, 'При вызове функции произошла ошибка')
+    return wrapper
 
 # def reverse_args(func):
 #         def wrapper(*args, **kwargs):
