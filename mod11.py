65482ef077921594c79f324e4ff3554d8f9609df
Berendal <amonar89@gmail.com>
2023-03-12 17:41:06 +0300

    re2end

--- mod11.py
+++ mod11.py
@@ -1,3 +1,76 @@
+# import sys
+# from collections import defaultdict
+# from bs4 import BeautifulSoup
+
+
+# d = defaultdict(set)
+# for line in sys.stdin:
+#     for tag in BeautifulSoup(line, "html.parser")():
+#         d[tag.name] |= set(tag.attrs)
+# for k, v in sorted(d.items()):
+#     print(k + ':', ", ".join(sorted(v)))
+
+
+
+# import re, sys
+
+
+# data = [line.strip() for line in sys.stdin]
+# for i in data:
+#     for j in (re.findall(r'<a.*href="(.*)">(.*)</a', i)):
+#         print(*j, sep=', ')
+
+
+# def abbreviate(phrase):
+#     return ''.join(re.findall(r'\b\w|[A-Z]', phrase)).upper()
+
+
+# print(len(re.findall(fr'\b{input()[:-2]}u?r\b', input(), re.I)))
+
+
+# print(len(re.findall(fr'\b({input()[:-2]}\w*)\b', input(), re.I)))
+
+
+# text = input()
+# print(len(re.findall(fr'\b{input()}\b', text)))
+
+
+# text = input()
+# print(len(re.findall(fr'\B{input()}\B', text)))
+# or
+# n = input()
+# res = 0
+# for _ in re.findall(fr'\B{(input())}\B', n):
+#     res += 1
+# print(res)
+
+
+# article = '''Stepik (до августа 2016 года Stepic) — это образовательная платформа и конструктор онлайн-курсов!
+
+# Первые образовательные материалы были выпущены на Stepik 3 сентября 2013 года.
+# В январе 2016 года Stepik выпустил мобильные приложения под iOS и Android. В 2017 году разработаны мобильные приложения для изучения ПДД в адаптивном режиме для iOS и Android...
+
+# На октябрь 2020 года на платформе зарегистрировано 5 миллионов пользователей!
+# Stepik позволяет любому зарегистрированному пользователю создавать интерактивные обучающие уроки и онлайн-курсы, используя видео, тексты и разнообразные задачи с автоматической проверкой и моментальной обратной связью. 
+
+# Проект сотрудничает как с образовательными учреждениями, так и c индивидуальными преподавателями и авторами.  
+# Stepik сегодня предлагает онлайн-курсы от образовательных организаций, а также индивидуальных авторов!
+
+# Система автоматизированной проверки задач Stepik была использована в ряде курсов на платформе Coursera, включая курсы по биоинформатике от Калифорнийского университета в Сан-Диего и курс по анализу данных от НИУ «Высшая школа экономики»...
+
+# Stepik также может функционировать как площадка для проведения конкурсов и олимпиад, среди проведённых мероприятий — отборочный этап Олимпиады НТИ (2016—2020) (всероссийской инженерной олимпиады школьников, в рамках программы Национальная технологическая инициатива), онлайн-этап акции Тотальный диктант в 2017 году, соревнования по информационной безопасности StepCTF-2015...'''
+
+# word = 0
+# es = 0
+# for w in re.findall(r'(^Stepik)', article, re.I | re.M):
+#     word += 1
+# for e in re.findall(r'(\.\.\.|!)$', article, re.M):
+#     es += 1
+
+# print(word, es, sep='\n')
+
+
+
 # import sys, re
 
 
